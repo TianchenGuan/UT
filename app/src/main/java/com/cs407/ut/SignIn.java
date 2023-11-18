60c52ced5f576afdaf279823f31a8dc6caf382c2
TianchenGuan <144616071+TianchenGuan@users.noreply.github.com>
2023-11-17 20:04:14 -0600

    Added login

--- app/src/main/java/com/cs407/ut/SignIn.java
+++ app/src/main/java/com/cs407/ut/SignIn.java
@@ -7,22 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Button signInButton = findViewById(R.id.btnLogin); // Replace 'buttonSignIn' with your button's ID
+        setContentView(R.layout.activity_sign_in);
+        Button signInButton = findViewById(R.id.buttonSignIn); // Replace 'buttonSignIn' with your button's ID
 
         // Set OnClickListener
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an Intent to start the SignIn activity
-                Intent intent = new Intent(Login.this, SignIn.class);
+                Intent intent = new Intent(SignIn.this, MainActivity.class);
                 startActivity(intent);
             }
         });
     }
+
 }
---
+++ app/src/main/java/com/cs407/ut/SignUp.java
@@ -0,0 +1,4 @@
+package com.cs407.ut;
+
+public class SignUp {
+}
