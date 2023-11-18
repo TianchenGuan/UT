b77b0dd5fa4ba084abfc13d7bf6424120983bba3
sleepyPolarbear <101209142+sleepyPolarbear@users.noreply.github.com>
2023-11-17 18:30:16 -0600

    build the Category page, and user can go to this page by clicking home page "see all" button at categories part

--- app/src/main/java/com/cs407/ut/Login.java
+++ app/src/main/java/com/cs407/ut/Login.java
@@ -6,9 +6,5 @@ import android.os.Bundle;
 
 public class Login extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+
 }
